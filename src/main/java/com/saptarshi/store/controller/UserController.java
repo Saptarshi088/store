b49c13861cd6cfea1266af9d1ef4f4371df73a36
Saptarshi <saptarshibiswas6@gmail.com>
2026-01-02 18:58:20 +0530

    Added feature to return sorted User data via UserController Get("/users/**") mapping

--- src/main/java/com/saptarshi/store/controller/UserController.java
+++ src/main/java/com/saptarshi/store/controller/UserController.java
@@ -5,9 +5,13 @@ import com.saptarshi.store.entities.User;
 import com.saptarshi.store.mappers.UserMapper;
 import com.saptarshi.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 
 @RestController
@@ -17,8 +21,11 @@ public class UserController {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     @GetMapping
-    public Iterable<UserDto> getAllUsers(){
-        return userRepository.findAll()
+    public List<UserDto> getAllUsers(@RequestParam(required = false, defaultValue = "name" , name = "sort") String sort){
+        if(!Set.of("name","email").contains(sort)){
+            sort = "name";
+        }
+        return userRepository.findAll(Sort.by(sort))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
--- src/main/java/com/saptarshi/store/dto/UserDto.java
+++ src/main/java/com/saptarshi/store/dto/UserDto.java
@@ -1,12 +1,14 @@
 package com.saptarshi.store.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @JsonIgnore
     private Long id;
     private String name;
     private String email;

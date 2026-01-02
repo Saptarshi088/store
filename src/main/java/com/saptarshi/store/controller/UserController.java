1bd3028e37111b9be7ded28ceacc9fe1fb6563ea
Saptarshi <saptarshibiswas6@gmail.com>
2026-01-02 12:25:19 +0530

    Added more endpoints and Used ResponseEntity instead of returning an instance of an Entity

--- src/main/java/com/saptarshi/store/controller/UserController.java
+++ src/main/java/com/saptarshi/store/controller/UserController.java
@@ -1,18 +1,13 @@
 package com.saptarshi.store.controller;
 
+import com.saptarshi.store.dto.UserDto;
 import com.saptarshi.store.entities.User;
 import com.saptarshi.store.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Iterator;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,18 +16,21 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public Iterable<User> getAllUsers(){
-        return userRepository.findAll();
+    public Iterable<UserDto> getAllUsers(){
+        return userRepository.findAll()
+                .stream()
+                .map(user->new UserDto(user.getId(),user.getName(),user.getEmail()))
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id){
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id){
         User user = userRepository.findById(id).orElse(null);
         if(user==null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
 //        return new ResponseEntity<>(user,HttpStatus.OK);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(new UserDto(user.getId(),user.getName(),user.getEmail()));
     }
 }

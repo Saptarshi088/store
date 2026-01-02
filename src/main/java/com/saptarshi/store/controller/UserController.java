b12f6d3bc050f2752f91f1e33d6494c0f513f479
Saptarshi <saptarshibiswas6@gmail.com>
2026-01-02 12:11:08 +0530

    Added more endpoints and Used ResponseEntity instead of returning an instance of an Entity

--- src/main/java/com/saptarshi/store/controller/UserController.java
+++ src/main/java/com/saptarshi/store/controller/UserController.java
@@ -3,7 +3,12 @@ package com.saptarshi.store.controller;
 import com.saptarshi.store.entities.User;
 import com.saptarshi.store.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Iterator;
@@ -11,11 +16,23 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        User user = userRepository.findById(id).orElse(null);
+        if(user==null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+//        return new ResponseEntity<>(user,HttpStatus.OK);
+        return ResponseEntity.ok(user);
     }
 }

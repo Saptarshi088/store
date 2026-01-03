334d45b0ea592c3cd12c6c8195126fc7db21f899
Saptarshi <saptarshibiswas6@gmail.com>
2026-01-03 22:15:07 +0530

    Added a post and delete endpoint for Product

--- src/main/java/com/saptarshi/store/controller/ProducrtController.java
+++ src/main/java/com/saptarshi/store/controller/ProducrtController.java
@@ -3,12 +3,14 @@ package com.saptarshi.store.controller;
 import com.saptarshi.store.dto.ProductDto;
 import com.saptarshi.store.entities.Product;
 import com.saptarshi.store.mappers.ProductMapper;
+import com.saptarshi.store.repositories.CategoryRepository;
 import com.saptarshi.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Set;
@@ -19,6 +21,7 @@ import java.util.Set;
 public class ProducrtController {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
 
     @GetMapping("")
@@ -41,5 +44,34 @@ public class ProducrtController {
             return ResponseEntity.ok(productMapper.toDto(product));
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto productDto,
+            UriComponentsBuilder uriBuilder) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var product = productMapper.toEntity(productDto);
+        product.setCategory(category);
+        productRepository.save(product);
+        productDto.setId(product.getId());
+
+        var uri = uriBuilder.path("/products/{id}").buildAndExpand(productDto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(productDto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        var product = productRepository.findById(id).orElse(null);
+        if(product==null)
+            return ResponseEntity.badRequest().build();
+        productRepository.delete(product);
+        return ResponseEntity.ok().build();
     }
 }
